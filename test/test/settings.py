58020c11a20fad824b3c3c75053aeced46b500fb
Fernando Borretti <eudoxiahp@gmail.com>
2015-03-03 13:55:58 -0200

    Add ML settings

--- test/test/settings.py
+++ test/test/settings.py
@@ -1,17 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for test project
-#
-# For simplicity, this file contains only the most important settings by
-# default. All the other settings are documented here:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#
-
 BOT_NAME = 'test'
 
 SPIDER_MODULES = ['test.spiders']
 NEWSPIDER_MODULE = 'test.spiders'
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'test (+http://www.yourdomain.com)'
+MONKEYLEARN_CLASSIFIER = 'abc'
+MONKEYLEARN_AUTH_TOKEN = '123'
+MONKEYLEARN_CLASSIFIER_FIELDS = ['title', 'desc']
+MONKEYLEARN_CATEGORIES_FIELD = 'category'
