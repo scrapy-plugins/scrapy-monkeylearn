25d932d3ce7bfd63ec009571432045342a862773
Fernando Borretti <eudoxiahp@gmail.com>
2015-03-03 10:14:10 -0200

    Extract configuration

--- scrapy_monkeylearn.py
+++ scrapy_monkeylearn.py
@@ -1,5 +1,8 @@
 CLASSIFY_TEXT_URL = 'https://app.monkeylearn.com/api/v1/categorizer/%s/classify_text/'
 
+ML_CLASSIFIER = 'MONKEYLEARN_CLASSIFIER'
+ML_AUTH = 'MONKEYLEARN_AUTH_TOKEN'
+
 def classify_text(classifier, text, token):
     return requests.post(
         CLASSIFY_TEXT_URL % classifier,
@@ -14,8 +17,13 @@ def classify_text(classifier, text, token):
 class MonkeyLearnPipeline(object):
     """A pipeline to classify items."""
 
-    def __init__(self):
-        pass
+    classifier = None
+    auth_token = None
+
+    def __init__(self, crawler):
+        super(MonkeyLearnPipeline, self).__init__()
+        self.classifier = crawler.settings[ML_CLASSIFIER]
+        self.auth_token = crawler.settings[ML_AUTH]
 
     def process_item(self, item, spider):
         return item
