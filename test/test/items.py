57acb7d8ebbbda26009ab81999e35f0d5ed203b0
Fernando Borretti <eudoxiahp@gmail.com>
2015-03-03 13:57:23 -0200

    Add test item

--- test/test/items.py
+++ test/test/items.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class TestItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    desc = scrapy.Field()
+    category = scrapy.Field()
