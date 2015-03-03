7dca03e16f8bf84e952cf60fecb8cfc9c11137bd
Fernando Borretti <eudoxiahp@gmail.com>
2015-03-03 10:21:16 -0200

    Add a stub README

--- scrapy_monkeylearn.py
+++ scrapy_monkeylearn.py
@@ -2,6 +2,8 @@ CLASSIFY_TEXT_URL = 'https://app.monkeylearn.com/api/v1/categorizer/%s/classify_
 
 ML_CLASSIFIER = 'MONKEYLEARN_CLASSIFIER'
 ML_AUTH = 'MONKEYLEARN_AUTH_TOKEN'
+ML_CLASSIFY_FIELDS = 'MONKEYLEARN_CLASSIFIER_FIELDS'
+ML_CATEGORY_FIELD = 'MONKEYLEARN_CATEGORY_FIELD'
 
 def classify_text(classifier, text, token):
     return requests.post(
@@ -21,9 +23,11 @@ class MonkeyLearnPipeline(object):
     auth_token = None
 
     def __init__(self, crawler):
+        "Constructor, extract configuration information."
         super(MonkeyLearnPipeline, self).__init__()
         self.classifier = crawler.settings[ML_CLASSIFIER]
         self.auth_token = crawler.settings[ML_AUTH]
 
     def process_item(self, item, spider):
+        "Classify an item."
         return item
