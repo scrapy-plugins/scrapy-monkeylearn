8451d2bcf012dac056f4d903d84f4fd5c85a7e3f
Fernando Borretti <eudoxiahp@gmail.com>
2015-03-03 10:07:34 -0200

    Add a simple setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='scrapy-monkeylearn',
+    version='0.1',
+    description='MonkeyLearn pipeline for Scrapy',
+    author='Fernando Borretti',
+    author_email='fernando@tryolabs.com',
+    packages=['scrapy_monkeylearn'],
+)
