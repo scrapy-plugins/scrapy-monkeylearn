7606706e17d4886dad5f277508dac4275dce26dc
Fernando Borretti <eudoxiahp@gmail.com>
2015-03-04 12:30:57 -0200

    Change 'length' to 'len'

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ class TestSimple(unittest.TestCase):
         reactor.run()
 
     def test_item(self):
-        self.assertEqual(length(DB), 1)
+        self.assertEqual(len(DB), 1)
 
 if __name__ == '__main__':
     unittest.main()
