ab410cb134a93cbd6d0d0235fb5fcbb0e7d6e06a
Fernando Borretti <eudoxiahp@gmail.com>
2015-03-04 11:42:52 -0200

    Run spider in the tests

--- test.py
+++ test.py
@@ -1,15 +1,28 @@
 import unittest
 
+import os
+
 from twisted.internet import reactor
 from scrapy.crawler import Crawler
 from scrapy import log, signals
+from scrapy.settings import Settings
 from test.spiders.test_spider import TestSpider
-from scrapy.utils.project import get_project_settings
 
 class TestSimple(unittest.TestCase):
 
-    def test_shuffle(self):
-        self.assertEqual(1, 1)
+    def setUp(self):
+        os.environ.setdefault('SCRAPY_SETTINGS_MODULE', 'myprojectdir.settings')
+        self.spider = TestSpider()
+        self.crawler = Crawler(Settings())
+        self.crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
+        self.crawler.configure()
+
+
+    def test_run(self):
+        self.crawler.crawl(self.spider)
+        self.crawler.start()
+        log.start()
+        reactor.run()
 
 if __name__ == '__main__':
     unittest.main()
