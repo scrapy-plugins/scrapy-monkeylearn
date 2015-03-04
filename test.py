24e5f02e1c401e91d30ca64c358e327533512b55
Fernando Borretti <eudoxiahp@gmail.com>
2015-03-04 12:48:26 -0200

    Don't run the spider programmatically, configure settings in test.py

--- test.py
+++ test.py
@@ -5,28 +5,26 @@ from twisted.internet import reactor
 from scrapy.crawler import Crawler
 from scrapy import log, signals
 from scrapy.settings import Settings
-from test.test.spiders.test_spider import TestSpider
-from test.test.pipelines import DB
+
+from scrapy_monkeylearn import MonkeyLearnPipeline
 
 
 class TestSimple(unittest.TestCase):
 
     def setUp(self):
-        os.environ.setdefault('SCRAPY_SETTINGS_MODULE', 'test.settings')
-        self.spider = TestSpider()
-        self.crawler = Crawler(Settings())
-        self.crawler.signals.connect(reactor.stop,
-                                     signal=signals.spider_closed)
+        # Configure settings
+        settings = Settings()
+        settings.set('MONKEYLEARN_CLASSIFIER', 'abc')
+        settings.set('MONKEYLEARN_AUTH_TOKEN', '123')
+        settings.set('MONKEYLEARN_CLASSIFIER_FIELDS', ['title', 'desc'])
+        settings.set('MONKEYLEARN_CATEGORIES_FIELD', 'category')
+        settings.set('MONKEYLEARN_DEBUG', True)
+
+        self.crawler = Crawler(settings)
         self.crawler.configure()
 
-    def test_run(self):
-        self.crawler.crawl(self.spider)
-        self.crawler.start()
-        log.start()
-        reactor.run()
-
-    def test_item(self):
-        self.assertEqual(len(DB), 1)
+    def test_create_pipeline(self):
+        self.pipeline = MonkeyLearnPipeline(self.crawler)
 
 if __name__ == '__main__':
     unittest.main()
