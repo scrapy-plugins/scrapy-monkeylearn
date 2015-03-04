7f72fdfb6474a63eddf883d9c8ac18a9b1a1ef1b
Fernando Borretti <eudoxiahp@gmail.com>
2015-03-04 11:01:25 -0200

    Enable the ML pipeline

--- test/test/settings.py
+++ test/test/settings.py
@@ -10,3 +10,7 @@ MONKEYLEARN_AUTH_TOKEN = '123'
 MONKEYLEARN_CLASSIFIER_FIELDS = ['title', 'desc']
 MONKEYLEARN_CATEGORIES_FIELD = 'category'
 MONKEYLEARN_DEBUG = True
+
+ITEM_PIPELINES = {
+    'scrapy_monkeylearn.MonkeyLearnPipeline': 300,
+}
