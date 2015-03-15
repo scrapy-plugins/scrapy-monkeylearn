26045b171fd69044dbb090b123a73310ea2154c4
Juan Riaza <github@juanriaza.com>
2015-03-15 11:06:55 +0100

    bump

--- scrapy_monkeylearn.py
+++ scrapy_monkeylearn.py
@@ -1,35 +1,16 @@
 # -*- coding: utf-8 -*-
-
-"""This module implements a Scrapy Pipeline that extracts text from Scrapy
-Items, classifies them using MonkeyLearn, then stores the category in the Item.
-
-"""
-
-import requests
 import json
+import scrapy
 
-CLASSIFY_TEXT_URL = 'https://api.monkeylearn.com/api/v1/categorizer/%s/classify_text/'
+from scrapy.exceptions import NotConfigured
+
+
+CLASSIFY_TEXT_URL = 'https://api.monkeylearn.com/api/v1/categorizer/{classifier}/classify_text/'
 
 ML_CLASSIFIER = 'MONKEYLEARN_CLASSIFIER'
 ML_AUTH = 'MONKEYLEARN_AUTH_TOKEN'
 ML_CLASSIFY_FIELDS = 'MONKEYLEARN_CLASSIFIER_FIELDS'
 ML_CATEGORIES_FIELD = 'MONKEYLEARN_CATEGORIES_FIELD'
-ML_DEBUG = 'MONKEYLEARN_DEBUG'
-
-
-def _classify_text(classifier, token, text):
-    """Send a request to MonkeyLearn to classify some text."""
-    resp = requests.post(
-        CLASSIFY_TEXT_URL % classifier,
-        headers={
-            'Authorization': 'Token ' + token,
-            'Content-Type': 'application/json'
-        },
-        data=json.dumps({
-            'text': text
-        })
-    )
-    return resp.json()['result']
 
 
 class ConfigError(Exception):
@@ -52,25 +33,30 @@ class ConfigError(Exception):
 class MonkeyLearnPipeline(object):
     """A pipeline to classify items."""
 
-    classifier = None
-    auth_token = None
-    classifier_fields = None
-    category_field = None
+    log_header = '[MonkeyLearnPipeline] '
 
     @classmethod
     def from_crawler(cls, crawler):
-        return cls(crawler.settings)
+        return cls(crawler)
 
-    def __init__(self, settings):
+    def __init__(self, crawler):
         "Constructor."
         super(MonkeyLearnPipeline, self).__init__()
+        self.crawler = crawler
 
         # Extract configuration
-        self.classifier = settings[ML_CLASSIFIER]
-        self.auth_token = settings[ML_AUTH]
-        self.classifier_fields = settings[ML_CLASSIFY_FIELDS]
-        self.categories_field = settings[ML_CATEGORIES_FIELD]
-        self.debug = settings[ML_DEBUG]
+        self.classifier = crawler.settings.get(ML_CLASSIFIER)
+        self.auth_token = crawler.settings.get(ML_AUTH)
+        self.classifier_fields = crawler.settings.get(ML_CLASSIFY_FIELDS)
+        self.categories_field = crawler.settings.get(ML_CATEGORIES_FIELD)
+
+        # Check required options
+        if not all((
+                self.classifier,
+                self.auth_token,
+                self.classifier_fields,
+                self.categories_field)):
+            raise NotConfigured('missing options')
 
         # Validate options
         if not isinstance(self.classifier, str):
@@ -94,18 +80,43 @@ class MonkeyLearnPipeline(object):
                               found_type=type(self.category_field))
 
     def process_item(self, item, spider):
-        "Classify an item."
-        # Extract fields
-        text_to_classify = ' '.join([item[field_name]
+        request = self._make_request(item, spider)
+        dfd = self.crawler.engine.download(request, spider)
+        dfd.addCallback(self.check_response, item)
+        dfd.addErrback(self.handle_error, item)
+        return dfd
+
+    def _make_request(self, item, spider):
+        text_to_classify = u' '.join([item[field_name]
                                      for field_name in self.classifier_fields])
-        # Classify item
-        if self.debug:
-            categories = [{'label': 'None'}]
-        else:
-            categories = _classify_text(classifier=self.classifier,
-                                        token=self.auth_token,
-                                        text=text_to_classify)
-        # Store category
+        body = json.dumps({'text': text_to_classify})
+        request = scrapy.Request(
+            CLASSIFY_TEXT_URL.format(classifier=self.classifier),
+            method='POST',
+            headers={
+                'Authorization': 'Token {}'.format(self.auth_token),
+                'Content-Type': 'application/json'},
+            body=body)
+        self.crawler.stats.inc_value('monkeylearn_api/requests_count')
+        return request
+
+    def check_response(self, response, item):
+        self.crawler.stats.inc_value('monkeylearn_api/response_count')
+        self.crawler.stats.inc_value(
+            'monkeylearn_api/response_status_count/{}'.format(response.status))
+        if response.status != 200:
+            scrapy.log.msg(
+                self.log_header % 'Non 200 response from api {0}'.format(response.url),
+                scrapy.log.ERROR)
+            return item
+
+        json_data = json.loads(response.body)
+        categories = json_data['result']
         item[self.categories_field] = categories
-        # Return item
+        return item
+
+    def handle_error(self, failure, item):
+        message = 'error when parsing api response {0} Traceback'.format(item.get('url'))
+        message += failure.getBriefTraceback()
+        scrapy.log.err(self.log_header + message)
         return item
